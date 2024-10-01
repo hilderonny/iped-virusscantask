6267dc91ea61bd8ded49410884e79966ba46e075
hilderonny <hilderonny2014@gmail.com>
2024-10-01 14:39:02 +0200

    Updated to using new task bridge

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # iped-virusscantask
 
 [IPED](https://github.com/sepinf-inc/IPED) task for virus scanning via shared workers
+
+# iped-virusscantask
+
+[IPED](https://github.com/sepinf-inc/IPED) task for virus scanning via distributed workers. Uses [Task Bridge](https://github.com/hilderonny/taskbridge) together with [ScanForVirus](https://github.com/hilderonny/taskworker-scanforvirus) worker for distributing and doing the work.
+
+## Output
+
+Using this task each file will get the following additional metadata.
+
+|Property|Description|
+|-|-|
+|`virusscan:status`|`FOUND``when a virus was detected and `OK` when the file is clean.|
+|`virusscan:detection`|Name of the detected virus. If no virus was found, this element is empty.|
+
+## Installation
+
+First download an install [IPED](https://github.com/sepinf-inc/IPED).
+
+Next copy the file `scripts/tasks/VirusScanningTask.py` into the `scripts/tasks` folder of your IPED installation.
+
+Copy the file `conf/VirusScanning.txt` into the `conf` directory of your IPED installation.
+
+In your IPED folder open the file `IPEDConfig.txt` and add the following line.
+
+```
+enableVirusScanning = true
+```
+
+Finally open the file `conf/TaskInstaller.xml` and look for a line containing `iped.engine.task.ParsingTask`. Add the following line after this line:
+
+```xml
+<task script="VirusScanningTask.py"></task>
+```
+
+## Configuration
+
+The configuration is done in the file `conf/VirusScanning.txt` in your IPED directory. This files contains comments on how to setup the connection to the task bridge.
--- scripts/tasks/VirusScanningTask.py
+++ scripts/tasks/VirusScanningTask.py
@@ -4,20 +4,11 @@
 # enabling: enableVirusScanning
 # Config file: VirusScanning.txt
 
-import os
 import time
 import json
-import shutil
 import requests
 
-# Configuration properties
-enableProp = "enableVirusScanning"
-configFile = "VirusScanning.txt"
-forensicTaskBridgeApiUrlProp = "forensicTaskBridgeApiUrl"
-forensicTaskBridgeShareDirectoryProp = "forensicTaskBridgeShareDirectory"
-
 API_URL = None
-API_SHARE_DIR = None
 
 def readJsonFile(file_path):
     with open(file_path, "r", encoding="utf-8") as json_file:
@@ -34,41 +25,23 @@ class VirusScanningTask:
 
     def getConfigurables(self):
         from iped.engine.config import DefaultTaskPropertiesConfig
-        return [DefaultTaskPropertiesConfig(enableProp, configFile)]
+        return [DefaultTaskPropertiesConfig("enableVirusScanning", "VirusScanning.txt")]
 
     def init(self, configuration):
-        taskConfig = configuration.getTaskConfigurable(configFile)
+        taskConfig = configuration.getTaskConfigurable("VirusScanning.txt")
         VirusScanningTask.enabled = taskConfig.isEnabled()
         if not VirusScanningTask.enabled:
             return
         extraProps = taskConfig.getConfiguration()
-        global API_URL, API_SHARE_DIR
-        API_URL = extraProps.getProperty(forensicTaskBridgeApiUrlProp)
-        API_SHARE_DIR = extraProps.getProperty(forensicTaskBridgeShareDirectoryProp)
+        global API_URL
+        taskBridgeUrl = extraProps.getProperty("taskBridgeUrl")
+        if not taskBridgeUrl.endswith("/"):
+            taskBridgeUrl = f"{taskBridgeUrl}/"
+        API_URL = f"{taskBridgeUrl}api/"
 
     def finish(self):
         return
         
-    # Process an Item object. This method is executed on all case items.
-    # It can access any method of Item class and store results as a new extra attribute.
-    #
-    #  Some Getters:
-    #  String:  getName(), getExt(), getType(), getPath(), getHash(), getMediaType().toString(), getCategories() (categories separated by | )
-    #  Date:    getModDate(), getCreationDate(), getAccessDate() (podem ser nulos)
-    #  Boolean: isDeleted(), isDir(), isRoot(), isCarved(), isSubItem(), isTimedOut(), hasChildren()
-    #  Long:    getLength()
-    #  Metadata getMetadata()
-    #  Object:  getExtraAttribute(String key) (returns an extra attribute)
-    #  String:  getParsedTextCache() (returns item extracted text, if this task is placed after ParsingTask)
-    #  File:    getTempFile() (returns a temp file with item content)
-    #  BufferedInputStream: getBufferedInputStream() (returns an InputStream with item content)
-    #
-    #  Some Setters: 
-    #           setToIgnore(boolean) (ignores the item and excludes it from processing and case)
-    #           setAddToCase(boolean) (inserts or not item in case, after being processed: default true)
-    #           addCategory(String), removeCategory(String), setMediaTypeStr(String)
-    #              setExtraAttribute(key, value), setParsedTextCache(String)
-    #
     def process(self, item):
         item_name = item.getName()
         # Process only if not already in cache, therefor hashing must be enabled
@@ -77,45 +50,58 @@ class VirusScanningTask:
             return
 
         result = self.process_via_api(item, hash)
-
+        
         if "status" in result:
             item.setExtraAttribute("virusscan:status", result["status"])
-        if "virus" in result:
-            item.setExtraAttribute("virusscan:virus", result["virus"])
-        logger.info("Processed item %s: %s", item_name, result)
-
-    # Result format
-    #   status
-    #   virus
+        if "detection" in result:
+            item.setExtraAttribute("virusscan:detection", result["detection"])
+        logger.info(f"Processed item {item_name}: {result}")
 
     def process_via_api(self, item, hash):
         result = {}
-        # Copy file to share folder
-        source_file_path = item.getTempFile().getAbsolutePath()
-        share_file_path = os.path.join(API_SHARE_DIR, hash)
-        shutil.copy(source_file_path, share_file_path)
-        # Add virus scanning task
-        response = requests.post(f"{API_URL}tasks/scanforvirus/add/{hash}/clamav")
-        if response.status_code != 200:
-            logger.error(f"Cannot access {API_URL}tasks/scanforvirus/add/{hash}/clamav")
+
+        scanforvirus_fp = open(item.getTempFile().getAbsolutePath(), "rb")
+        scanforvirus_files = { "file" : scanforvirus_fp }
+        scanforvirus_json = { "type" : "scanforvirus" }
+
+        # Add scanforvirus task and upload file
+        add_scanforvirus_response = requests.post(f"{API_URL}tasks/add/", files=scanforvirus_files, data={ "json" : json.dumps(scanforvirus_json) })
+        if add_scanforvirus_response.status_code != 200:
+            result["error"] = "Error adding scanforvirus task"
             return result
-        add_virusscan_json_result = response.json()
-        #print(add_virusscan_json_result)
-        virusscan_task_id = add_virusscan_json_result["id"]
-        # Wait for completion
-        while requests.get(f"{API_URL}tasks/status/{virusscan_task_id}").json()["status"] != "done":
-            time.sleep(5)
-        virusscan_result = requests.get(f"{API_URL}tasks/result/{virusscan_task_id}").json()
-        #print(virusscan_result)
-        if "error" in virusscan_result["result"]:
-            result["error"] = virusscan_result["result"]["error"]
-        else:
-            # Collect results
-            result["status"] = virusscan_result["result"]["status"]
-            if "virus" in virusscan_result["result"]:
-                result["virus"] = virusscan_result["result"]["virus"]
-        # Delete task from bridge
-        delete_result = requests.delete(f"{API_URL}tasks/remove/{virusscan_task_id}")
-        #print(delete_result)
-        print(result)
+        task_scanforvirus_id = add_scanforvirus_response.json()["id"]
+
+        # Wait for scanforvirus task completion
+        task_scanforvirus_completed = False
+        while not task_scanforvirus_completed:
+            status_scanforvirus_response = requests.get(f"{API_URL}tasks/status/{task_scanforvirus_id}")
+            if status_scanforvirus_response.status_code != 200:
+                result["error"] = "Error requesting scanforvirus task status"
+                return result
+            status_scanforvirus = status_scanforvirus_response.json()["status"]
+            if status_scanforvirus == "completed":
+                task_scanforvirus_completed = True
+            else:
+                time.sleep(3)
+
+        # Request scanforvirus result
+        result_scanforvirus_response = requests.get(f"{API_URL}tasks/result/{task_scanforvirus_id}")
+        if result_scanforvirus_response.status_code != 200:
+            result["error"] = "Error requesting scanforvirus task result"
+            return result
+        scanforvirus_result = result_scanforvirus_response.json()
+
+        # Delete scanforvirus task
+        delete_scanforvirus_response = requests.delete(f"{API_URL}tasks/remove/{task_scanforvirus_id}")
+        if delete_scanforvirus_response.status_code != 200:
+            result["error"] = "Error deleting scanforvirus task"
+            return result
+
+        if "error" in scanforvirus_result["result"]:
+            result["error"] = scanforvirus_result["result"]["error"]
+        if "status" in scanforvirus_result["result"]:
+            result["status"] = scanforvirus_result["result"]["status"]
+        if "detection" in scanforvirus_result["result"]:
+            result["detection"] = scanforvirus_result["result"]["detection"]
+
         return result
